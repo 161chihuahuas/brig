1c754c1dacdfbcda681106ee242d7f5eded3e7aa
Lily Anne <lily.anne.hall@protonmail.com>
2025-06-14 22:01:32 -0700

    tests pass sometimes, there is a race bug somewhere

--- test/brig.e2e.js
+++ test/brig.e2e.js
@@ -3,17 +3,19 @@
 const { PassThrough } = require('node:stream');
 const { randomUUID } = require('node:crypto');
 const { expect } = require('chai');
-const { events, consensus } = require('..');
+const { events, consensus, roles } = require('..');
 const { ElectionTimeout } = require('../lib/events');
 
 
 describe('@module brig/consensus', function() {
 
-  const NODES = 8;
+  const NODES = 3;
   const PEERS = [];
   const CONSENSUS = [];
 
-  before(function() {
+  this.timeout(8000);
+
+  before(function(done) {
     for (let i = 0; i < NODES; i++) {
       PEERS.push(new consensus.Peer(randomUUID(), new PassThrough({
         objectMode: true
@@ -30,6 +32,29 @@ describe('@module brig/consensus', function() {
 
       CONSENSUS.push(new consensus.Consensus(peer.id, peers));
     }
+
+    /* really verbose logs
+    for (let c = 0; c < CONSENSUS.length; c++) {
+      CONSENSUS[c].on(events.Debug, messages => {
+        console.log(
+          `[ ${CONSENSUS[c].id} ]`,
+          ...messages
+        );
+      });
+    }
+    */
+    function leaderElected() {
+      return CONSENSUS.map(c => {
+        return c.state.currentRole;
+      }).includes(roles.Leader);
+    }
+
+    const waitForLeader = setInterval(function() {
+      if (leaderElected()) {
+        clearInterval(waitForLeader);
+        done();
+      }
+    }, 100);
   });
 
   describe('~Consensus', function() {
@@ -42,10 +67,33 @@ describe('@module brig/consensus', function() {
       CONSENSUS[0].once(events.ElectionTimeout, done);
     });
 
-    it('broadcasts log entry to peers', function() {
-      CONSENSUS[0].emit(events.Broadcast, {
-        hello: 'brig'
+    it('broadcasts a log entry to peers to replicate', function(done) {
+      CONSENSUS[0].broadcast({
+        genesis: 'entry'
       });
+      setTimeout(() => {
+        CONSENSUS.forEach(c => {
+          expect(c.state.log.entries).to.have.lengthOf(1);
+          expect(c.state.log.entries[0].payload.genesis).to.equal('entry');
+        });
+        done()
+      }, 1000);
+    });
+
+    it('can sync state transitions from all nodes', function(done) {
+      this.timeout(3000 * CONSENSUS.length + 200);
+
+      for (let i = 0; i < CONSENSUS.length; i++) {
+        setTimeout(() => {
+          CONSENSUS[i].broadcast({ publisher: CONSENSUS[i].id });
+        }, Math.ceil(Math.random() * 300));
+      }
+      setTimeout(() => {
+        CONSENSUS.forEach(c => {
+          expect(c.state.log.entries).to.have.lengthOf(CONSENSUS.length + 1);
+        });
+        done();
+      }, 3000 * CONSENSUS.length);
     });
 
   });
